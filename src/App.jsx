ce7784fed99cb166333db3eabc3cf25542cd233f
Satyamrk <satyamrk18@gmail.com>
2025-07-19 23:20:25 +0530

    react routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Product from "./pages/product.jsx";
+import AddProduct from "./pages/AddProduct";
 function App() {
   return (
     <div>
-      <h1>hello worls</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/addproduct" element={<AddProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
