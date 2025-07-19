02ea5050db6d7c03a67d603496cc7065426fbcea
Satyamrk <satyamrk18@gmail.com>
2025-07-19 23:41:29 +0530

    navbar basic styling added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       {/* ita all gos to the search bar as pages input */}
-      <Link to="/">Home</Link>{" "}
-      <Link to="/addproduct">Add Product</Link>
-      <Link to="/product">Product</Link>
+      <Link to="/" className="nav-item">Home</Link>
+      <Link to="/addproduct" className="nav-item">Add Product</Link>
+      <Link to="/product" className="nav-item">Product</Link>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Navbar from "./../components/navbar.jsx"
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
       <Navbar />
     </div>
   )
