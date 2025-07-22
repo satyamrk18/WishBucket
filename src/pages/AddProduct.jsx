2efc073aa17f8a5bac6efe635afcd0a7eb97af7f
Satyamrk <satyamrk18@gmail.com>
2025-07-22 13:08:17 +0530

    add product form content added

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,23 +1,53 @@
-import React from 'react'
-import Navbar from "./../components/navbar.jsx"
-import "./AddProduct.css"
+import React from "react";
+import Navbar from "./../components/navbar.jsx";
+import "./AddProduct.css";
+import { useState, useEffect } from "react";
 const AddProduct = () => {
+  const [today, setToday] = useState("");
+  useEffect(() => {
+    const date = new Date();
+    const formatedDate = date.toISOString().split("T")[0];
+    setToday(formatedDate);
+  }, []);
+
   return (
     <div>
-           <Navbar />
-     <div className='addproduct'>
-       <div className='sideform'></div>
-       <div className='cardview'>
-        {/* main card */}
-   <div className='card'>
- 
+      <Navbar />
+      <div className="addproduct">
+        <div className="sideform">
+          <form>
+            <label>Product Name</label>
+            <input type="text" placeholder="Product Name" name="product name" />
+            <label>Product Image</label>
+            <input
+              type="text"
+              placeholder="inter product image URL"
+              name="product image url"
+            />
+            {/* more image feature remain */}
+            <label>Product Price ₹</label>
+            <input
+              type="number"
+              placeholder="Product price"
+              name="product price"
+            />
+            <lable>Product Link</lable>
+            <input type="text" placeholder="Product Link" name="product Link" />
+            <label>Date Of Manifestation</label>
+            <input type="date" defaultValue={today} name="Manifestation"/>
+            <label>Expected Date To Achive</label>
+            <input type="date" name="Expected Date Of achivement" />
 
-   </div>
-
-       </div>
-     </div>
+            <button>Submit</button>
+          </form>
+        </div>
+        <div className="cardview">
+          {/* main card */}
+          <div className="card"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
