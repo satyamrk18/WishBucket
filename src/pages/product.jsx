60e4ab1c74d9f38fe02871f903b6fda069009e4f
Satyamrk <satyamrk18@gmail.com>
2025-07-24 22:09:33 +0530

    product render to the produscts page

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Navbar from "./../components/navbar.jsx";
+import { useEffect, useState } from "react";
 const product = () => {
+
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const products =JSON.parse( localStorage.getItem("products") || []);
+    setProducts(products)
+  }, []);
   return (
     <div>
       <Navbar />
       <h1>Product page</h1>
+     <div className="card">
+     {
+      products.map((allproduct)=>
+      {
+          const {title,image,link,price,priority,today,archivedate,description} = allproduct;
+          return (
+            <div><img src={image}/></div>
+          )
+      })
+     }
+     </div>
     </div>
   );
 };
