a0052297571e5cb78ca77fe584018fab72aba10b
Satyamrk <satyamrk18@gmail.com>
2025-07-27 19:56:32 +0530

    value cards added

---
+++ src/components/valuecards.jsx
@@ -0,0 +1,23 @@
+import { Radius } from "lucide-react";
+import React from "react";
+
+const valuecards = ({ title, value }) => {
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "start",
+    height: "auto",
+    width: "350px",
+    padding: "20px",
+    border: "2px solid rgba(128, 128, 128, 0.272)",
+    borderRadius:"10px",
+  };
+  return (
+    <div style={style}>
+      <h3>{title}</h3>
+      <h1>{value}</h1>
+    </div>
+  );
+};
+
+export default valuecards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Navbar from "./../components/navbar.jsx"
-import "./Home.css"
+import React from "react";
+import Navbar from "./../components/navbar.jsx";
+import ValueCards from "./../components/valuecards.jsx";
+import "./Home.css";
 const Home = () => {
   return (
-    <div className='container'>
-       <Navbar />
-      <h1>Home page</h1>
+    <div className="container">
+      <Navbar />
+      <div className="valuecards-contianer">
+        <ValueCards title="Total Items" value={10} />
+        <ValueCards title="Total Price" value={10} />{" "}
+        {/* price rendering remain */}
+        <ValueCards title="Total Items" value={10} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
