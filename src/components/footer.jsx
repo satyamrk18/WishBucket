1e1b70ee394c0cded738a86b35d85124762dc7a5
Satyamrk <satyamrk18@gmail.com>
2025-07-28 23:35:13 +0530

    social media icon edit and linking complete

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { Github, Linkedin, MailPlus, CircleParking } from "lucide-react";
 const footer = () => {
   return (
-    <div className='footer'>
-        <p className='footer-tagline'>Must Visit</p>
-      <div className='socialmedia-container'>github linkedin peerlist </div>
+    <div className="footer">
+      <div className="socialmedia-container">
+       <a href="https://github.com/satyamrk18" target="new">
+          <Github className="social-icon"/>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/satyam-katkade-4ba389279/"
+          target="new"
+        >
+          <Linkedin className="social-icon"/>
+        </a>
+        <a href="mailto:satyamrk18@gmail.com?subject=Hello%20Satyam&body=This%20is%20a%20test%20mail%20from%20my%20app" target="new">
+          <MailPlus className="social-icon"/>
+        </a>
+        <a href="https://peerlist.io/satyamrk18" target="new"><CircleParking  className="social-icon"/></a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
